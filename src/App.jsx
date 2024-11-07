78c9570e909d3ee8051ba3e82322bd1e78d4c08a
GujiGventsadze <gventsadzeguji@gmail.com>
2024-11-07 17:41:32 +0400

    Optimization

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Homepage />} />
+          <Route path="/EGportfolio" element={<Homepage />} />
           <Route path="/projects" element={<Projectspage />} />
           <Route path="/career" element={<Careerpage />} />
           <Route path="/contact" element={<Contactpage />} />
